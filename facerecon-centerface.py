421b10686e7b7cb6b04eb86d14655f27509c24ba
andre-fu <andrefu.af@hotmail.com>
2020-11-17 23:24:25 -0500

    small fix

--- facerecon-centerface.py
+++ facerecon-centerface.py
@@ -3,13 +3,14 @@ import scipy.io as sio
 import os
 from centerface import CenterFace
 import uuid
+import sys
 
 class FaceRecognition(): 
-    def __init__(self, video: str, fullpic=False):
+    def __init__(self, video: str,  start, end, fullpic=False):
         self.vidcap = cv2.VideoCapture(video)
         self.fullpic = fullpic
-        sec = 0
-        frameRate = 1 #//it will capture image in each 0.5 second
+        sec = start
+        frameRate = 1/2 #//it will capture image in each 0.5 second
         count=1
         success = self._getFrame(sec, count)
         while success:
@@ -17,6 +18,9 @@ class FaceRecognition():
             sec = sec + frameRate
             sec = round(sec, 2)
             success = self._getFrame(sec, count)
+            
+            if sec >= end:
+                break
     
     def _getFrame(self, sec, count):
         self.vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
@@ -41,4 +45,15 @@ class FaceRecognition():
         return hasFrames	
 
 
-fr = FaceRecognition('tbbt_science.mp4')
+from time import time
+start_time = float(sys.argv[1])
+end_time = float(sys.argv[2])
+videoName = str(sys.argv[3])
+# start_time = 0
+# end_time = 30
+print (start_time, end_time)
+
+start = time()
+fr = FaceRecognition(videoName, start_time, end_time)
+
+print (time()-start)
